f0d3995d3b75bf65817c6798ed520b8833929a36
hanszemsftcsae <112930448+hanszemsftcsae@users.noreply.github.com>
2023-08-09 15:22:34 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
 
+terraform {
+  backend "azurerm" {
+    storage_account_name = "abcd1234"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+    use_azuread_auth     = true
+    subscription_id      = "00000000-0000-0000-0000-000000000000"
+    tenant_id            = "00000000-0000-0000-0000-000000000000"
+  }
+}
+
